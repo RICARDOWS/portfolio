3efcb7d290a4451b8f3c10651999e4190e73ba0a
RICARDOWS <44734657+RICARDOWS@users.noreply.github.com>
2024-02-27 12:31:12 -0700

    improve theme colors and toggle theme feature

---
+++ src/i18n/indexPage.ts
@@ -0,0 +1,27 @@
+export const LANDING_PAGE = {
+    es: {
+      'meta.description': '',
+      'meta.title': "",
+      'meta.keywords': "",
+      'meta.og:image': '',
+      'meta.og:site_name': "" ,
+      'meta.og:locale': 'es_ES',
+
+  
+      'footer.la_velada_of_year': '© ',
+      'footer.all_rights_reserved': 'Todos los derechos reservados.',
+      'footer.instagram_logo': 'Logo de Instagram',
+    },
+    en: {
+      'meta.description': '',
+      'meta.title': "",
+      'meta.keywords': "",
+      'meta.og:image': '',
+      'meta.og:site_name': "" ,
+      'meta.og:locale': '',
+  
+      'footer.la_velada_of_year': '© ',
+      'footer.all_rights_reserved': 'All rights reserved.',
+      'footer.instagram_logo': 'Instagram Logo',
+    }
+  } as const
